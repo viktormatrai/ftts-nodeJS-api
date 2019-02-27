e4e6c848697e2c6ef156fba595404faffb6d3ce0
Viktor Matrai <viktor0matrai@gmail.com>
2019-02-27 15:02:32 +0100

    user controller, with sign in/up/out

---
+++ API/controller/userController.js
@@ -0,0 +1,102 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const user = require('../models/user');
+exports.signUp = (req, res, next) => {
+    user.find({ email: req.body.email})
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: "this email is already in use"
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err){
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new user({
+                            _id: new mongoose.Types.ObjectId,
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user.save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: "user created"
+                                });
+                            })
+                            .catch(err => {
+                               console.log(err);
+                               res.status(500).json({
+                                   error: err
+                               });
+                            });
+                    }
+                });
+            }
+        });
+};
+
+exports.logIn = (res, req, next) => {
+  user.findOne({ email: req.body.email})
+      .exec()
+      .then(user => {
+          if (user.length < 1) {
+              return res.status(401).json({
+                  message: 'Authentication failed'
+              });
+          }
+          bcrypt.compare(req.body.password, user.password, (err, result) => {
+              if (err) {
+                  return res.status(401).json({
+                      message: 'Authentication failed'
+                  });
+              }
+              if (result) {
+                  const token = jwt.sign({
+                      email: user.email,
+                      userId: user._id
+                    },
+                    process.env.JWT_KEY,
+                      {
+                          expiresIn: '1h'
+                      });
+                  return res.status(200).json({
+                      message: 'Authenticated',
+                      token: token
+                  });
+              }
+          });
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json({
+              error: err
+          });
+      });
+};
+
+exports.deleteUser = (req, res, next) => {
+  user.removeOne({_id: req.params.userId})
+      .exec()
+      .then(result=> {
+          res.status(200).json({
+              message: "user deleted"
+          });
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json({
+              error: err
+          });
+      });
+};
+
+exports.logOut = (req, res, next) => {
+
+};
