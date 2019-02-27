c5f6a5027cf8bf89e12bafcfeae7bb14865c6acd
Viktor Matrai <viktor0matrai@gmail.com>
2019-02-27 13:47:34 +0100

    app.js with cors, atlas connection essentials

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const mongoAtlasURIStr = 'mongodb://viktormatrai:' + process.env.MONGO_ATLAS_PW +
+                        '@ftts-shard-00-00-ut7wu.mongodb.net:27017,ftts-shard-00-01-ut7wu.mongodb.net:27017,' +
+                        'ftts-shard-00-02-ut7wu.mongodb.net:27017/test?ssl=true&replicaSet=' +
+                        'FTTS-shard-0&authSource=admin&retryWrites=true';
 
+const raceRoutes = require('./API/routes/race');
 
-app.use((res, res, next) => {
-    res.status(200).json({
+mongoose.connect(mongoAtlasURIStr, {useNewUrlParser: true, dbName: 'FTTS'});
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-    });
+app.use('/', raceRoutes);
+
+app.use((req, res, next) => {
+   const error = new Error("Not found");
+   error.status = 404;
+   next(error);
 });
+
+app.use((err, req, res, next) => {
+   res.status(err.status || 500);
+   console.log(err);
+   res.json({
+       error: {
+           message: err.message
+       }
+   });
+});
+
+module.exports = app;
