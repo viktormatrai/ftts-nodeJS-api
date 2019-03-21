ea6843928ae54f43b80a980bd9cc4b08973f9563
Viktor Matrai <viktor0matrai@gmail.com>
2019-03-21 17:40:03 +0100

    unnecesarry route delete

--- API/routes/team.js
+++ API/routes/team.js
@@ -7,7 +7,6 @@ router.get('/', teamController.getAllTeams);
 router.post('/new-team', auth, teamController.createTeam);
 router.post('/assign/:teamId', auth, teamController.assignToTeam);
 // router.get('/:teamId', teamController.getTeamById);
-// router.post('/assign', teamController.assignToTeam);
 // router.delete(':teamId, auth, teamController.deleteTeam)
 
 module.exports = router;
