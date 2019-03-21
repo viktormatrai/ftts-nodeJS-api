1994fbc539998632f7e881980aa8e6a05e56093d
Viktor Matrai <viktor0matrai@gmail.com>
2019-03-21 15:48:20 +0100

    team assigns, and resign from old team

---
+++ API/sevice/teamService.js
@@ -0,0 +1,35 @@
+const Team = require('../models/team');
+
+exports.assignToTeam = (req, res) => {
+    const {teamId} = req.params;
+    const {userId} = req.userData;
+    removeFromOldTeamIfAssigned(userId);
+
+    Team.updateOne({_id: teamId}, {$addToSet: {racers: userId}})
+        .exec()
+        .then(result => {
+            res.status(201).json({
+                message: "team updated",
+                request: {
+                    type: "GET",
+                    url: `http://localhost:9999/teams/${teamId}`
+                }
+            })
+        })
+        .catch(error =>{
+            res.status(500).json({
+                error
+            })
+        });
+};
+
+const removeFromOldTeamIfAssigned = (userId) => {
+    Team.find({racers: userId})
+        .exec()
+        .then(result => {
+            if (result.length > 0) {
+               Team.updateOne({_id: result[0]._id}, {$pull: {racers: userId}})
+                   .exec()
+           }
+        })
+};
