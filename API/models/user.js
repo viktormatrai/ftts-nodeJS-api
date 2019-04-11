a2eb4369119ab046abc6dccc1dda0c2faa80571a
Viktor Matrai <viktor0matrai@gmail.com>
2019-04-11 22:30:45 +0200

    user model update

--- API/models/user.js
+++ API/models/user.js
@@ -7,14 +7,17 @@ const users = mongoose.Schema({
     lastName: {type: String, required: true},
     nickName: {type: String},
     gender: {type: Number, max: 1},
-    team: {type: mongoose.Schema.Types.ObjectId, ref: 'team'},
+    team: {
+            id: {type: mongoose.Schema.Types.ObjectId, ref: 'team'},
+            teamName: String
+    },
     races: {type: mongoose.Schema.Types.ObjectId, ref: 'race'},
     email: {
             type: String,
             required: true,
             unique: true,
             match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-        },
+    },
     password: {type: String, required: true},
     points: {type: Number, default: 0},
     tag: {type: mongoose.Schema.Types.ObjectId, ref: 'tag'}
