fb66ab4ec9fe58f756deab84cfb95798b6251a42
Viktor Matrai <viktor0matrai@gmail.com>
2019-04-11 09:32:41 +0200

    registration from form

--- API/routes/race.js
+++ API/routes/race.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const raceController = require('../controller/raceController');
+const racerTimeController = require('../controller/racerTimeController');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 
@@ -10,6 +11,15 @@ router.post("/", auth, admin, raceController.createRace);
 router.patch("/:raceId", auth, admin, raceController.updateRace);
 router.delete('/:raceId', auth, admin, raceController.deleteRace);
 router.post('/assign-to-race/:raceId', auth, raceController.assignToRace);
+
+//racerTime related routes
+router.get('/times/:raceId', racerTimeController.getTimesByRaceId);
+router.post('/create-time/:race/:racer', auth, admin, racerTimeController.createStartingTime);
+router.post('/finishing-time/:timeId', auth, admin, racerTimeController.setFinishingTime);
+router.post('/neutral-zones/:timeId', auth, admin, racerTimeController.setNeutralTimes);
+router.post('/dnf/:timeId', auth, admin, racerTimeController.setIfDNF);
+router.post('/penalty/:timeId', auth, admin, racerTimeController.setPenalty);
+router.post('/save-final-time/:timeId', auth, admin, racerTimeController.saveFinalTime);
 
 
 module.exports = router;
